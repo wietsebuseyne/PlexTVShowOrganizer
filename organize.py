062df11676d0aa3f4dda4dab8d31bd684596b400
Wietse Buseyne <wietsebuseyne@hotmail.com>
2017-01-16 21:40:55 +0100

    Removed MediaFile.dll dependency

--- organize.py
+++ organize.py
@@ -9,13 +9,46 @@ import json
 import string
 import glob
 from shutil import copy2, move
-from pymediainfo import MediaInfo
+#from pymediainfo import MediaInfo
+
+video_file_extensions = [
+'.264', '.3g2', '.3gp', '.3gp2', '.3gpp', '.3gpp2', '.3mm', '.3p2', '.60d', '.787', '.89', '.aaf', '.aec', '.aep', '.aepx',
+'.aet', '.aetx', '.ajp', '.ale', '.am', '.amc', '.amv', '.amx', '.anim', '.aqt', '.arcut', '.arf', '.asf', '.asx', '.avb',
+'.avc', '.avd', '.avi', '.avp', '.avs', '.avs', '.avv', '.axm', '.bdm', '.bdmv', '.bdt2', '.bdt3', '.bik', '.bin', '.bix',
+'.bmk', '.bnp', '.box', '.bs4', '.bsf', '.bvr', '.byu', '.camproj', '.camrec', '.camv', '.ced', '.cel', '.cine', '.cip',
+'.clpi', '.cmmp', '.cmmtpl', '.cmproj', '.cmrec', '.cpi', '.cst', '.cvc', '.cx3', '.d2v', '.d3v', '.dat', '.dav', '.dce',
+'.dck', '.dcr', '.dcr', '.ddat', '.dif', '.dir', '.divx', '.dlx', '.dmb', '.dmsd', '.dmsd3d', '.dmsm', '.dmsm3d', '.dmss',
+'.dmx', '.dnc', '.dpa', '.dpg', '.dream', '.dsy', '.dv', '.dv-avi', '.dv4', '.dvdmedia', '.dvr', '.dvr-ms', '.dvx', '.dxr',
+'.dzm', '.dzp', '.dzt', '.edl', '.evo', '.eye', '.ezt', '.f4p', '.f4v', '.fbr', '.fbr', '.fbz', '.fcp', '.fcproject',
+'.ffd', '.flc', '.flh', '.fli', '.flv', '.flx', '.gfp', '.gl', '.gom', '.grasp', '.gts', '.gvi', '.gvp', '.h264', '.hdmov',
+'.hkm', '.ifo', '.imovieproj', '.imovieproject', '.ircp', '.irf', '.ism', '.ismc', '.ismv', '.iva', '.ivf', '.ivr', '.ivs',
+'.izz', '.izzy', '.jss', '.jts', '.jtv', '.k3g', '.kmv', '.ktn', '.lrec', '.lsf', '.lsx', '.m15', '.m1pg', '.m1v', '.m21',
+'.m21', '.m2a', '.m2p', '.m2t', '.m2ts', '.m2v', '.m4e', '.m4u', '.m4v', '.m75', '.mani', '.meta', '.mgv', '.mj2', '.mjp',
+'.mjpg', '.mk3d', '.mkv', '.mmv', '.mnv', '.mob', '.mod', '.modd', '.moff', '.moi', '.moov', '.mov', '.movie', '.mp21',
+'.mp21', '.mp2v', '.mp4', '.mp4v', '.mpe', '.mpeg', '.mpeg1', '.mpeg4', '.mpf', '.mpg', '.mpg2', '.mpgindex', '.mpl',
+'.mpl', '.mpls', '.mpsub', '.mpv', '.mpv2', '.mqv', '.msdvd', '.mse', '.msh', '.mswmm', '.mts', '.mtv', '.mvb', '.mvc',
+'.mvd', '.mve', '.mvex', '.mvp', '.mvp', '.mvy', '.mxf', '.mxv', '.mys', '.ncor', '.nsv', '.nut', '.nuv', '.nvc', '.ogm',
+'.ogv', '.ogx', '.osp', '.otrkey', '.pac', '.par', '.pds', '.pgi', '.photoshow', '.piv', '.pjs', '.playlist', '.plproj',
+'.pmf', '.pmv', '.pns', '.ppj', '.prel', '.pro', '.prproj', '.prtl', '.psb', '.psh', '.pssd', '.pva', '.pvr', '.pxv',
+'.qt', '.qtch', '.qtindex', '.qtl', '.qtm', '.qtz', '.r3d', '.rcd', '.rcproject', '.rdb', '.rec', '.rm', '.rmd', '.rmd',
+'.rmp', '.rms', '.rmv', '.rmvb', '.roq', '.rp', '.rsx', '.rts', '.rts', '.rum', '.rv', '.rvid', '.rvl', '.sbk', '.sbt',
+'.scc', '.scm', '.scm', '.scn', '.screenflow', '.sec', '.sedprj', '.seq', '.sfd', '.sfvidcap', '.siv', '.smi', '.smi',
+'.smil', '.smk', '.sml', '.smv', '.spl', '.sqz', '.ssf', '.ssm', '.stl', '.str', '.stx', '.svi', '.swf', '.swi',
+'.swt', '.tda3mt', '.tdx', '.thp', '.tivo', '.tix', '.tod', '.tp', '.tp0', '.tpd', '.tpr', '.trp', '.ts', '.tsp', '.ttxt',
+'.tvs', '.usf', '.usm', '.vc1', '.vcpf', '.vcr', '.vcv', '.vdo', '.vdr', '.vdx', '.veg','.vem', '.vep', '.vf', '.vft',
+'.vfw', '.vfz', '.vgz', '.vid', '.video', '.viewlet', '.viv', '.vivo', '.vlab', '.vob', '.vp3', '.vp6', '.vp7', '.vpj',
+'.vro', '.vs4', '.vse', '.vsp', '.w32', '.wcp', '.webm', '.wlmp', '.wm', '.wmd', '.wmmp', '.wmv', '.wmx', '.wot', '.wp3',
+'.wpl', '.wtv', '.wve', '.wvx', '.xej', '.xel', '.xesc', '.xfl', '.xlmv', '.xmv', '.xvid', '.y4m', '.yog', '.yuv', '.zeg',
+'.zm1', '.zm2', '.zm3', '.zmv'  ]
 
 help_string = 'organize.py -o <outputdirectory> [-i <inputdirectory>] [-n <replacement_names>] [-m]'
 
 sub_extensions_regex = '(srt|sub|sbv|smi|ssa|ass|vtt|pgs|vobsub|idx)'
 
 os.environ['PATH'] = sys.path[0] + ';' + os.environ['PATH']
+
+def is_video(filename):
+	return os.path.splitext(filename)[1] in video_file_extensions
 
 def filename_to_serie(filename, name_replacements = None):
 	p = re.compile('(.+)(s)(\d\d?)e(\d\d?).*', re.IGNORECASE)
@@ -120,17 +153,18 @@ def main():
 	for dirname, dirnames, filenames in os.walk(args.input):
 		for filename in filenames:
 			original_path = os.path.join(dirname, filename)
-			fileInfo = MediaInfo.parse(original_path)
-			for track in fileInfo.tracks:
-				if track.track_type == "Video":
-					videos_total += 1
-					serie = filename_to_serie(filename, args.names)
-					if not serie is None:
-						move_file(serie, original_path, args.output, not args.move)
-						series_found += 1
-					else:
-						unrecognized_files.append(filename)
-					break
+			if is_video(original_path):
+			#fileInfo = MediaInfo.parse(original_path)
+			#for track in fileInfo.tracks:
+			#	if track.track_type == "Video":
+				videos_total += 1
+				serie = filename_to_serie(filename, args.names)
+				if not serie is None:
+					move_file(serie, original_path, args.output, not args.move)
+					series_found += 1
+				else:
+					unrecognized_files.append(filename)
+				break
 	print('\nOrganizing media library finished.')
 	print('Total video files encountered:	' + str(videos_total))
 	print('TV-shows found and moved:		' + str(series_found))
